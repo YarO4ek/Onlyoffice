27f10ffa111523803591d335d651ee8ea86e2b6d
Yaroslav Maslov <yaroslavmaslov0951@gmail.com>
2023-10-09 14:52:39 +0200

    fix

--- ELKA/__tests__/tree.test.js
+++ ELKA/__tests__/tree.test.js
@@ -5,12 +5,12 @@ describe("createTree", () => {
         const treeHeight = 7;
         const treeText = createTree(treeHeight);
 
-        // Проверяем, что елка имеет правильное количество строк
+
         const treeLines = treeText.trim().split("\n");
         expect(treeLines.length).toBe(treeHeight + 2);
 
 
-        // Проверяем, что последняя строка состоит из "TTTTT"
+
         const lastLine = treeLines[treeHeight];
         expect(lastLine.trim()).toBe("TTTTT");
 
@@ -22,10 +22,10 @@ describe("createTree", () => {
         const filePath = "tree.txt";
         createTree(treeHeight);
 
-        // Проверяем, что файл был создан
+
         expect(fs.existsSync(filePath)).toBe(true);
 
-        // Проверяем, что содержимое файла соответствует ожиданиям
+        
         const fileContent = fs.readFileSync(filePath, "utf-8");
         expect(fileContent).toEqual(createTree(treeHeight));
 
